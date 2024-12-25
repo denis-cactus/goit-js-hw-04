8a36703f25be109031c23eef537218eaa4be2580
Denis Mahei <m.play.sys@gmail.com>
2024-12-25 19:57:07 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,1 @@
-function slugify(text) {
-  return text.toLowerCase().trim().replace(/\s+/g, '-');
-}
 
-console.log(slugify(' Arrays for beginners')); // "arrays-for-beginners"
-console.log(slugify(' English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
